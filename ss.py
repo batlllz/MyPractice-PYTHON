332a5c897e43f3fc19fa3528f32b60ace399d7fb
batlllz <batool.alshukaili7@gmail.com>
2023-07-25 15:15:57 +0400

    ShapeSpace

--- ss.py
+++ ss.py
@@ -1,2 +1,21 @@
-print("batoul")
-print("my first code")
+        
+h= 5
+l=4
+PI=3.14
+r=2
+Tr=(0.5*h*l)
+SQ=(l*4)
+CI=PI*r**2
+
+print(Tr+SQ-CI)
+           
+           
+           
+print(len("python program"))         
+print(chr(97))
+print(chr(96))        
+print ("*")
+print ("*" * 2)
+print ("*" * 3)
+print ("*" * 4 )
+
