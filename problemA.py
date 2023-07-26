442e8730a73569b768c7c0516481e86d02bcfda5
batlllz <batool.alshukaili7@gmail.com>
2023-07-26 16:58:24 +0400

    broblemD

---
+++ problemA.py
@@ -0,0 +1,4 @@
+a= int(input("speed "))
+b= int(input(" time "))
+s= a * b
+print (s)
---
+++ problemD.py
@@ -0,0 +1,11 @@
+x= int ( input (" enter size of storage : "))
+n= int ( input (" enter number of files : "))
+y= int ( input (" enter size of files : "))
+full_size = n*y
+if full_size <=x:
+    print (" yes can be stored ")
+else :
+    print (" sorry can not be stored")
+   
+
+
