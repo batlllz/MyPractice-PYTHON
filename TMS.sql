9cc300b9a98347090dae183cf09a6d1e7e557138
Batoul <138441388+batlllz@users.noreply.github.com>
2023-08-10 00:39:34 +0400

    Update TMS.sql

--- TMS.sql
+++ TMS.sql
@@ -42,10 +42,13 @@ Bk_RemCopies Numeric(5));
 
 create table BOOKTOPIC (
 Tp_BkID Numeric(6) ,
-Tp_Desc char(30) NOT NULL,
+Tp_Desc varchar(30) NOT NULL,
 Bk_Edition Numeric(2) ,
 constraint Tp_BkIDFK foreign key (Tp_BkID )REFERENCES BOOK(Bk_ID));
 
+SELECT* FROM BOOKTOPIC
+ALTER TABLE BOOKTOPIC 
+DROP Column Bk_Edition ;
 
 create table COURSE (
 Cr_Code char(8) PRIMARY KEY,--PRIMERY KEY COLUMN 
@@ -61,7 +64,7 @@ Li_usage char(1) constraint Li_usageTorR check (Li_usage= 'T' OR Li_usage='R' ))
 
 create table REGISTRATION (
 Re_BrID Numeric(6) constraint Re_BrIDFK foreign key (Re_BrID )REFERENCES BORROWER(Br_ID) ,
-Re_CrCode char(6)constraint Re_CrCodeFK foreign key (Re_CrCode) REFERENCES COURSE(Cr_Code) ,
+Re_CrCode char(8)constraint Re_CrCodeFK foreign key (Re_CrCode) REFERENCES COURSE(Cr_Code) ,
 Re_Semester char(6) NOT NULL );
 
 
@@ -69,4 +72,133 @@ create table ISSUING (
 is_BrID Numeric(6) constraint is_BrIDFK foreign key (is_BrID)REFERENCES BORROWER(Br_ID) ,
 is_BkID Numeric(6) constraint is_BkIDFK foreign key (is_BkID)REFERENCES BOOK(Bk_ID),
 is_DateTaken Date NOT NULL,
-is_DateReturn Date check(is_DateReturn>is_DateTaken));
+is_DateReturn Date, constraint issuning_ch_isDataReturn check(is_DateReturn >= is_DateTaken));
+
+
+
+
+INSERT INTO college VALUES('COM', 'Economy', 'Prof. Fahim');
+
+INSERT INTO college VALUES('SCI', 'Science', 'Prof. Salma');
+
+INSERT INTO college VALUES('EDU', 'Education', 'Dr. Hamad');
+
+INSERT INTO college VALUES('ART', 'Arts', 'Dr. Abdullah');
+
+
+
+INSERT INTO department VALUES('INFS','Information Systems','Dr. Kamla','COM');
+
+INSERT INTO department VALUES('FINA','Finance','Dr. Salim','COM');
+
+INSERT INTO department VALUES('COMP','Computer Science','Dr. Zuhoor','SCI');
+
+INSERT INTO department VALUES('BIOL','Biology','Dr. Othman','SCI');
+
+INSERT INTO department VALUES('HIST','History','Dr. Said','EDU');
+
+INSERT INTO department VALUES('CHEM', 'Chemistry', 'Dr. Alaa', 'SCI');
+
+
+
+INSERT INTO borrower VALUES (92120,'Ali','INFS',99221133,'Seeb','231','S');
+
+INSERT INTO borrower VALUES (10021,'Said','INFS',91212129,'Seeb','100','S');
+
+INSERT INTO borrower VALUES (10023,'Muna','FINA', NULL, 'Barka','12','S');
+
+INSERT INTO borrower VALUES (3000,'Mohammed','COMP',90000009,'Seeb','777','E');
+
+INSERT INTO borrower VALUES (4000,'Nasser','INFS',99100199,'Sur','11','E');
+
+
+
+INSERT INTO student VALUES(92120,'INFS',2012);
+
+INSERT INTO student VALUES(10021,'INFS',2015);
+
+INSERT INTO student VALUES(10023,'FINA',2015);
+
+
+
+INSERT INTO EMPLOYEEandINSTUCTOR VALUES(3000,'12',2221);
+
+INSERT INTO EMPLOYEEandINSTUCTOR VALUES(4000,'15',1401);
+
+
+
+INSERT INTO book VALUES(1001,'Database1',2,450,150,65);
+
+INSERT INTO book VALUES(1002,'Database2',3,300,100,100);
+
+INSERT INTO book VALUES(2001,'Intro. to Finanace',1,300,75,40);
+
+INSERT INTO book VALUES(3001,'Basic Op Mgmt',1,320,100,77);
+
+INSERT INTO book VALUES(3002,'Chemistry Book',2,500,100,80);
+
+INSERT INTO book VALUES(4001,'Biology',1,345,100,100);
+
+INSERT INTO book VALUES(3003,'Management I',2,560,44,34);
+
+INSERT INTO book VALUES(1003,'Java Prog.',3,555,50,50);
+
+
+
+INSERT INTO bookTopic VALUES (1001,'Basic DB Skills');
+
+INSERT INTO bookTopic VALUES (1001,'ERD');
+
+INSERT INTO bookTopic VALUES (1001,'EERD');
+
+INSERT INTO bookTopic VALUES (1002,'SQL');
+
+INSERT INTO bookTopic VALUES (1002,'Pl/SQL');
+
+INSERT INTO bookTopic VALUES (3001,'Management Skills');
+
+
+
+INSERT INTO course VALUES('COMP4201', 'Database1', 3, 1,'COMP');
+
+INSERT INTO course VALUES('COMP4202', 'Database2', 3, 2,'COMP');
+
+INSERT INTO course VALUES('BIOL1000', 'Intro. To Biology', 3, 5,'BIOL');
+
+INSERT INTO course VALUES('CHEM2000', 'Advanced Chemistry', 2, 2,'CHEM');
+
+
+
+INSERT INTO LINK VALUES('COMP4201',1001,'T');
+
+INSERT INTO LINK VALUES('COMP4201',1002,'R');
+
+INSERT INTO LINK VALUES('COMP4202',1002,'T');
+
+INSERT INTO LINK VALUES('BIOL1000',4001,'T');
+
+INSERT INTO LINK VALUES('CHEM2000',3002,'R');
+
+
+
+INSERT INTO REGISTRATION VALUES(92120,'COMP4201','FL2015');
+
+INSERT INTO REGISTRATION VALUES(10021,'COMP4202','FL2015');
+
+INSERT INTO REGISTRATION VALUES(92120,'BIOL1000','FL2015');
+
+INSERT INTO REGISTRATION VALUES(92120,'COMP4202','FL2016');
+
+INSERT INTO REGISTRATION VALUES(10021,'CHEM2000','FL2016');
+
+
+
+INSERT INTO issuing VALUES(92120, 1001, '01-Sep-2015', '30-Oct-2015');
+
+INSERT INTO issuing VALUES(10021, 1002, '30-Oct-2016', NULL);
+
+INSERT INTO issuing VALUES(92120, 1002, '21-Feb-2015', '01-Jan-2016');
+
+INSERT INTO issuing VALUES(92120, 3002, '30-Mar-2016', NULL);
+
+INSERT INTO issuing VALUES(10021, 3002, '01-Dec-2014', NULL);
